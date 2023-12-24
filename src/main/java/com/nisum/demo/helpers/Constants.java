b12ac572909a7e0dc6bad98fd1e893a182ecdc4d
Gustavo Alfredo Martinez <gustavo-9189@hotmail.com>
2023-12-24 17:59:02 -0300

    rollback to constants

--- src/main/java/com/nisum/demo/helpers/Constants.java
+++ src/main/java/com/nisum/demo/helpers/Constants.java
@@ -1,18 +1,9 @@
 package com.nisum.demo.helpers;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public class Constants {
 
-    @Value("${constants.authorization}")
-    public static String AUTHORIZATION;
-
-    @Value("${constants.bearer}")
-    public static String BEARER;
-
-    @Value("${constants.secret}")
-    public static String ACCESS_TOKEN_SECRET;
-
-    @Value("${constants.seconds}")
-    public static Long ACCESS_TOKEN_VALIDITY_SECONDS;
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String ACCESS_TOKEN_SECRET = "343f7d35-005b-48a9-bb5e-5b25dbbd638a";
+    public static final Long ACCESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
 }
