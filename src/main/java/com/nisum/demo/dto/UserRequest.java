717d448016cab093c6b2dd8cc8dd655bb3f9aff1
Gustavo Alfredo Martinez <gustavo-9189@hotmail.com>
2023-12-24 17:59:44 -0300

    validate fields request

--- src/main/java/com/nisum/demo/dto/UserRequest.java
+++ src/main/java/com/nisum/demo/dto/UserRequest.java
@@ -1,11 +1,20 @@
 package com.nisum.demo.dto;
 
+import jakarta.validation.constraints.Pattern;
 import java.util.List;
 
 public record UserRequest(
         String name,
+
+        @Pattern(
+                regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$",
+                message = "The email is not in the correct format")
         String email,
+
+        @Pattern(
+                regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$",
+                message = "The password has an incorrect format")
         String password,
+
         List<PhoneRequest> phones
-) {
-}
+) { }
