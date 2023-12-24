e2d81724556dc09b256529bd386dd6313b012bfb
Gustavo Alfredo Martinez <gustavo-9189@hotmail.com>
2023-12-24 16:00:55 -0300

    add tests

---
+++ src/test/java/com/nisum/demo/controller/UserControllerTest.java
@@ -0,0 +1,24 @@
+package com.nisum.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
