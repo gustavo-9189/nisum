a78eb85726ba22dd8cad74bb8799ad363933b95b
Gustavo Martinez <gustavo-9189@hotmail.com>
2024-01-04 17:48:20 -0300

    add enable jpa auditor config

---
+++ src/main/java/com/nisum/demo/config/AuditorConfig.java
@@ -0,0 +1,18 @@
+package com.nisum.demo.config;
+
+import com.nisum.demo.security.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditorConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+
+}
